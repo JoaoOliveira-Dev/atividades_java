be9a13510b242eca21491254eb9981ecf57fd620
JoaoOliveira-Dev <joaooli17@hotmail.comn>
2023-03-06 14:28:01 -0300

    teste de novo repositorio

--- src/atividade/Aluno.java
+++ src/atividade/Aluno.java
@@ -3,30 +3,30 @@ package atividade;
 
 public class Aluno implements Comparable<Aluno> {
 
-    String nome;
-    Double nota1;
-    Double nota2;
+	String nome;
+	Double nota1;
+	Double nota2;
 
-    public Double calculaMedia() {
-        return (nota1 + nota2) / 2;
-    }
+	public Double calculaMedia() {
+			return (nota1 + nota2) / 2;
+	}
 
-    public String situacaoAluno() {
-        if (calculaMedia() >= 6) {
-            return "Aprovado!";
-        } else {
-            return "Reprovado!";
-        }
-    }
+	public String situacaoAluno() {
+			if (calculaMedia() >= 6) {
+					return "Aprovado!";
+			} else {
+					return "Reprovado!";
+			}
+	}
 
-    @Override
-    public String toString() {
-        return nome + " - " + calculaMedia() + " - " + situacaoAluno() + "\n";
-    }
+	@Override
+	public String toString() {
+			return nome + " - " + calculaMedia() + " - " + situacaoAluno() + "\n";
+	}
 
-    @Override
-    public int compareTo(Aluno obj) {
-        return nome.compareTo(obj.nome);
-    }
+	@Override
+	public int compareTo(Aluno obj) {
+			return nome.compareTo(obj.nome);
+	}
 
 }
